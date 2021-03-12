fa464dff5b8ffb7c103f99bfbe854697ea90e29c
Nick Playfair <nick@playfair.dev>
2021-03-12 15:31:42 +0000

    Use express-validator and builtin body parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import nodemailer from 'nodemailer';
-import multiparty from 'multiparty';
 import emailConfig from './email.config';
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 8000;
 
 // Transporter
@@ -17,55 +18,47 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// Interfaces
-interface EmailData {
-  name: string;
-  email: string;
-  message: string;
-}
-
-interface Email {
-  from: string;
-  to: string;
-  subject: string;
-  text: string;
-}
-
 // Routes
-app.get('/', (req, res) => res.send('Access denied'));
+app.get('/', (req, res: express.Response) => res.send('Access denied'));
+app.get('/send', (req, res: express.Response) => res.send('Access denied'));
 
-app.post('/send', (req, res) => {
-  const form = new multiparty.Form();
-  const data: EmailData = {
-    name: '',
-    email: '',
-    message: '',
-  };
-  form.parse(req, (err, fields: EmailData) => {
-    if (err) {
-      res.status(500).send(`Error: ${err}`);
+app.post(
+  '/send',
+  body('name').exists({ checkNull: true }),
+  body('email').isEmail(),
+  body('message').isLength({ min: 5 }),
+  (req: express.Request, res: express.Response) => {
+    // Find validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    data.name = fields.name.toString();
-    data.email = fields.email.toString();
-    data.message = fields.message.toString();
 
+    // Construct email to send
+    interface Email {
+      from: string;
+      to: string;
+      subject: string;
+      text: string;
+    }
     const mail: Email = {
-      from: data.email,
+      from: req.body.email,
       to: emailConfig.email,
       subject: 'Contact form message from NP Electronics',
-      text: `${data.name} <${data.email}> \n${data.message}`,
+      text: `${req.body.name} <${req.body.email}> \n${req.body.message}`,
     };
 
     // Send email
     transporter.sendMail(mail, (err, info) => {
       if (err) {
-        res.status(500).send(`Error sending email: ${err}`);
-      } else {
-        res.status(200).send(`Email sent to ${info.accepted}`);
+        res.status(500).json(`Error sending email: ${err}`);
       }
+      return res.status(200).send(`Sent to ${info.accepted}`);
     });
-  });
-});
+
+    return res.status(200);
+  },
+);
 
 // Server
 app.listen(PORT, () => {
